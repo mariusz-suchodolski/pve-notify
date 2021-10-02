816126a90e16382f718ed1b1a95cdca96487f971
Mariusz Suchodolski <mariusz@suchodol.ski>
2021-10-02 16:51:54 +0200

    Create notify.py

---
+++ notify.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import mailparser
+import sys
+import apprise
+
+userIn = sys.stdin.read()
+
+mail = mailparser.parse_from_string(userIn)
+print(mail.text_plain[0])
+
+apobj = apprise.Apprise()
+apobj.add('')
+apobj.notify(body=mail.text_plain[0],title=mail.subject)
